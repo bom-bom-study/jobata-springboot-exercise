445470867ac7a6d121490e40036e5d754ea3a6f0
jo-bata <jobata@skuniv.ac.kr>
2019-03-11 20:04:08 +0900

    Initial commit

---
+++ src/test/java/com/jobata/springbootexercise/SpringbootExerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jobata.springbootexercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootExerciseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
